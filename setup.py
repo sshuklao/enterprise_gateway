a7d1af5bfbdee4847c60c367f5c18d609898ea7a
Luciano Resende <lresende@apache.org>
2019-08-11 12:46:20 -0700

    Update to Jupyter Kernel Gateway 2.4.0
    In preparation to enable Jupyter Notebook 6.0 there
    is a need to update to JKG 2.4.0 which has required
    NB6 compatibility fixes.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ Jupyter Notebooks to share resources across an Apache Spark cluster.
     install_requires=[
         'jupyter_client>=5.2.0',
         'jupyter_core>=4.4.0',
-        'jupyter_kernel_gateway>=2.3.0',
+        'jupyter_kernel_gateway>=2.4.0',
         'notebook>=5.7.6,<6.0',
         'paramiko>=2.1.2',
         'pexpect>=4.2.0',
